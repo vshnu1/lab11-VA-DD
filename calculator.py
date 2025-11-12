93ce561d460b726b908e644c32d69e1034516b11
Vishnu Araveeti <vishnuaraveeti@Vishnus-MacBook-Air.local>
2025-11-12 14:44:54 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -1,4 +1,11 @@
 import math 
+
+def square_root(a):
+    if a<0:
+        raise ValueError
+    return math.sqrt(a)
+def hypotenuse(a,b):
+    return math.hypot(a,b)
 def add(a,b): 
     return a+b 
 def sub(a,b): 
