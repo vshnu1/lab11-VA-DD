8fa83f3894b359194cd7d85ca216e8ee364ab646
Vishnu Araveeti <vishnuaraveeti@Vishnus-MacBook-Air.local>
2025-11-12 14:51:34 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -8,7 +8,7 @@ def hypotenuse(a,b):
     return math.hypot(a,b)
 def add(a,b): 
     return a+b 
-def sub(a,b): 
+def subtract(a,b): 
     return a-b 
 def mul(a,b): 
     return a*b 
@@ -16,7 +16,7 @@ def div(a,b):
     if a==0: 
         raise ZeroDivisionError 
     return b/a 
-def log(a,b): 
+def logarithm(a,b): 
     if a<=0 or b<=0: 
         raise ValueError 
     return math.log(b,a) 
